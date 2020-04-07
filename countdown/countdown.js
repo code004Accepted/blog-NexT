fee44ff8b477edf9efe28b871d2d8f06b6bf463f
code004 <code004@qq.com>
2020-04-07 11:16:55 +0800

    Site updated: 2020-04-07 11:15:15

---
+++ countdown/countdown.js
@@ -0,0 +1,20 @@
+function TimeDown(id, endDateStr) {
+    //结束时间
+    var endDate = new Date(endDateStr);
+    //当前时间
+    var nowDate = new Date();
+    //相差的总秒数
+    var totalSeconds = parseInt((endDate - nowDate) / 1000);
+    //天数
+    var days = Math.floor(totalSeconds / (60 * 60 * 24));
+    //取模（余数）
+    var modulo = totalSeconds % (60 * 60 * 24);
+    //小时数
+    var hours = Math.floor(modulo / (60 * 60));
+    modulo = modulo % (60 * 60);
+    //分钟
+    var minutes = Math.floor(modulo / 60);
+    //秒
+    var seconds = modulo % 60;
+    //输出到页面
+    if(totalSeconds
